b96b0af5941aaa52bee93da63a5fcadf7cf24da0
Harsh <harshmishra1132@gmail.com>
2025-10-03 17:26:15 +0530

    Add aggressive polyfill injection - Multiple layers of Request/Response polyfills to fix production build

--- ML_frontend/vite.config.js
+++ ML_frontend/vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Custom plugin to inject polyfills banner into bundle
+const polyfillBannerPlugin = () => {
+  return {
+    name: 'polyfill-banner',
+    transformIndexHtml(html) {
+      // Inject inline script at the very beginning
+      return html.replace(
+        '<head>',
+        `<head>
+    <script>
+      // Emergency polyfills injected by Vite
+      if (!globalThis.Request) globalThis.Request = window.Request = class Request{constructor(e,t){t=t||{},this.url="string"==typeof e?e:e?e.url:"",this.method=t.method||"GET",this.headers=t.headers||{},this.body=t.body}clone(){return new Request(this.url,this)}};
+      if (!globalThis.Response) globalThis.Response = window.Response = class Response{constructor(e,t){t=t||{},this.body=e,this.status=t.status||200,this.ok=this.status>=200&&this.status<300}clone(){return new Response(this.body,this)}json(){return Promise.resolve(JSON.parse(this.body))}};
+      if (!globalThis.Headers) globalThis.Headers = window.Headers = class Headers{constructor(e){this._headers={};if(e)for(var t in e)this._headers[t.toLowerCase()]=e[t]}get(e){return this._headers[e.toLowerCase()]}set(e,t){this._headers[e.toLowerCase()]=t}};
+    </script>`
+      );
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    polyfillBannerPlugin(),
     react()
   ],
   // Add polyfills for Node.js globals
